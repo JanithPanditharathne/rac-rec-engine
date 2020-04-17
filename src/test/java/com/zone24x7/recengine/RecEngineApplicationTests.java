c04c64cc6b6ba604b9b441cc83d3a4ec56682316
Nuwan Koggalahewa <nuwank@zone24x7.com>
2020-04-17 10:05:21 +0530

    Initial Commit

---
+++ src/test/java/com/zone24x7/recengine/RecEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zone24x7.recengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
